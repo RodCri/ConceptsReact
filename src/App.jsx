91323ed67a47dcac9a06c05373fceb459dc24bdd
RodCri <cristian.rodriguez1317@gmail.com>
2023-09-18 11:21:48 -0500

    Render collection with use map

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,56 @@
-import { Target } from './components/TargetCard/Target'
+//import { Target } from './components/TargetCard/Target'
 import './App.css'
-import { Counter } from './components/Counter/Counter'
-import { Giphy } from './components/giphyAPI/Giphy'
+//import { Counter } from './components/Counter/Counter'
+//import { Giphy } from './components/giphyAPI/Giphy'
 import { Following }  from './components/TwitterCard/Following'
 
+const usersData = [
+  {
+    name: 'Miguel Perez',
+    username: 'midudev',
+    isFollowing: false
+  },
+  {
+    name: 'Will Smith',
+    username: 'willsmith',
+    isFollowing: true
+  },
+  {
+    name: 'Elena Toro',
+    username: 'elenatoro',
+    isFollowing: false
+  }
+];
 export const App = () => {
   return (
     <>
+      {
+        usersData.map(user => {
+          const {name, username, isFollowing} = user;
+          return (
+            <Following name={name} username={username} initialFollowing={isFollowing} key={username} />
+          )
+        })
+      }
+      {/* 
+      <h2 className="title">Props and UseState</h2>
       <div className='cards'>
         <Target name="@rodCri" userId={1} contratado={false} description="FrontEnd Developer"/>
         <Target name="@fraCap" userId={2} contratado={true} description="BackEnd Developer"/>
         <Target name="@derRod" userId={3} contratado={true} description="QA"/>
       </div>
+      <h2 className="title">UseState Hook</h2>
       <div className="counter">
         <Counter aux={10}/>
       </div>
+      <h2 className="title">UseEffect Hook Api Fetch</h2>
       <Giphy />
       <h2 className="title">Uso de Props Renderizado condicional UseState</h2>
       <div className="container__following">
         <Following name='Miguel Perez' username='midudev' initialFollowing={false}/>
         <Following name='Will Smith' username='willsmith' initialFollowing/>
         <Following name='Elena Toro' username='elenatoro' initialFollowing={false}/>
-      </div>
+      </div> */}
     </>
   )
 }
